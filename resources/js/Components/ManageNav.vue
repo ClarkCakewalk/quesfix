ad7745e0aae1a7d5f872f32683ee8a6a8d1bb831
Clark in MBA <clark@alum.ccu.edu.tw>
2026-06-21 12:24:11 +0800

    增加資料管理頁面會到上層連結

--- resources/js/Components/ManageNav.vue
+++ resources/js/Components/ManageNav.vue
@@ -14,19 +14,28 @@ const tabs = [
 </script>
 
 <template>
-    <div class="mb-4 flex gap-1 border-b">
+    <div>
         <Link
-            v-for="t in tabs"
-            :key="t.route"
-            :href="route(t.route, question.id)"
-            class="-mb-px border-b-2 px-4 py-2 text-sm"
-            :class="
-                route().current(t.route)
-                    ? 'border-indigo-600 font-medium text-indigo-700'
-                    : 'border-transparent text-gray-500 hover:text-gray-800'
-            "
+            :href="route('questions.index')"
+            class="mb-3 inline-block text-sm text-gray-600 hover:underline"
         >
-            {{ t.label }}
+            ← 回專案列表
         </Link>
+
+        <div class="mb-4 flex gap-1 border-b">
+            <Link
+                v-for="t in tabs"
+                :key="t.route"
+                :href="route(t.route, question.id)"
+                class="-mb-px border-b-2 px-4 py-2 text-sm"
+                :class="
+                    route().current(t.route)
+                        ? 'border-indigo-600 font-medium text-indigo-700'
+                        : 'border-transparent text-gray-500 hover:text-gray-800'
+                "
+            >
+                {{ t.label }}
+            </Link>
+        </div>
     </div>
 </template>
